13883bef1caa42616578c81b95e9865e51748d5e
Matěj Outlý <matej.outly@clockstar.cz>
2015-07-29 13:39:52 +0100

    Pikaday installed without git repository

--- lib/generators/pikaday/install/install_generator.rb
+++ lib/generators/pikaday/install/install_generator.rb
@@ -29,9 +29,48 @@ gem 'momentjs-rails'
 			empty_directory("#{Dir.pwd}/vendor/assets/libraries/pikaday")
 		end
 
+		def create_tmp_directory
+			remove_dir("#{Dir.pwd}/vendor/assets/libraries/_pikaday")
+			empty_directory("#{Dir.pwd}/vendor/assets/libraries/_pikaday")
+		end
+
 		def clone_git_repository
-			run("git clone https://github.com/dbushell/Pikaday.git #{Dir.pwd}/vendor/assets/libraries/pikaday")
-			remove_dir("#{Dir.pwd}/vendor/assets/libraries/pikaday/.git")
+			run("git clone https://github.com/dbushell/Pikaday.git #{Dir.pwd}/vendor/assets/libraries/_pikaday")
+		end
+
+		def copy_files
+			
+			# JS
+			filename = nil
+			Dir.glob("#{Dir.pwd}/vendor/assets/libraries/_pikaday/pikaday.js") do |f|
+				filename = f
+			end
+			if !filename.nil?
+				copy_file(filename, "#{Dir.pwd}/vendor/assets/libraries/pikaday/pikaday.js")
+			end
+
+			# JS
+			filename = nil
+			Dir.glob("#{Dir.pwd}/vendor/assets/libraries/_pikaday/plugins/pikaday.jquery.js") do |f|
+				filename = f
+			end
+			if !filename.nil?
+				copy_file(filename, "#{Dir.pwd}/vendor/assets/libraries/pikaday/pikaday.jquery.js")
+			end
+
+			# CSS
+			filename = nil
+			Dir.glob("#{Dir.pwd}/vendor/assets/libraries/_pikaday/css/pikaday.css") do |f|
+				filename = f
+			end
+			if !filename.nil?
+				copy_file(filename, "#{Dir.pwd}/vendor/assets/libraries/pikaday/pikaday.css")
+			end
+
+		end
+
+		def cleanup
+			remove_dir("#{Dir.pwd}/vendor/assets/libraries/_pikaday")
 		end
 
 	end
