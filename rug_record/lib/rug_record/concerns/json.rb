44e5064c6b1d0cf9088815215f682c533b35fb6a
Matěj Outlý <matej.outly@clockstar.cz>
2015-11-12 19:04:21 +0100

    Active record supports ability to define additional methods automatically passed to JSON export

---
+++ rug_record/lib/rug_record/concerns/json.rb
@@ -0,0 +1,60 @@
+# *****************************************************************************
+# * Copyright (c) Clockstar s.r.o. All rights reserved.
+# *****************************************************************************
+# *
+# * JSON
+# *
+# * Author: Matěj Outlý
+# * Date  : 12. 11. 2015
+# *
+# *****************************************************************************
+
+require "active_record"
+
+module RugRecord
+	module Concerns
+		module Json extend ActiveSupport::Concern
+
+			module ClassMethods
+				
+				#
+				# Set JSON additional methods
+				#
+				def add_methods_to_json(methods)
+					if !methods.is_a? Array
+						methods = [methods]
+					end
+					if @json_additional_methods.nil?
+						@json_additional_methods = []
+					end
+					@json_additional_methods.concat(methods)
+				end
+
+				#
+				# Get JSON additional methods
+				#
+				def json_additional_methods
+					return @json_additional_methods
+				end
+
+			end
+
+			#
+			# Export model to JSON
+			#
+			def to_json(options = {})
+				if options[:methods].nil?
+					options[:methods] = self.class.json_additional_methods
+				elsif options[:methods].is_a? Array
+					options[:methods].concat(self.class.json_additional_methods)
+				else
+					options[:methods] = [options[:methods]].concat(self.class.json_additional_methods)
+				end
+				super(options)
+			end
+
+		end
+	end
+end
+
+ActiveRecord::Base.send(:include, RugRecord::Concerns::Json)
