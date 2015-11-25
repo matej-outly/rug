024a47cd9498fca7b8bab87ec9d368496c0350bb
Matěj Outlý <matej.outly@clockstar.cz>
2015-11-25 09:59:50 +0100

    Map builder supports marker and polygon labels

--- rug_builder/lib/rug_builder/map_builder.rb
+++ rug_builder/lib/rug_builder/map_builder.rb
@@ -53,17 +53,31 @@ module RugBuilder
 		end
 
 		#
-		# Render location
+		# Render marker
 		#
-		def location(name, latitude, longitude)
-			return "rug_map_#{@hash}.addMarker(#{latitude}, #{longitude})"
+		def marker(name, latitude, longitude)
+			return "rug_map_#{@hash}.addMarker('#{name.to_s}', #{latitude}, #{longitude});".html_safe
+		end
+
+		#
+		# Render marker label
+		#
+		def marker_label(name, content)
+			return "rug_map_#{@hash}.addMarkerLabel('#{name.to_s}', '#{content}');".html_safe
 		end
 
 		#
 		# Render polygon
 		#
 		def polygon(name, points)
-			return "rug_map_#{@hash}.addPolygon(#{points.to_json})"
+			return "rug_map_#{@hash}.addPolygon('#{name.to_s}', #{points.to_json});".html_safe
+		end
+
+		#
+		# Render polygon label
+		#
+		def polygon_label(name, content)
+			return "rug_map_#{@hash}.addPolygonLabel('#{name.to_s}', '#{content}');".html_safe
 		end
 
 	protected
@@ -73,44 +87,42 @@ module RugBuilder
 
 			js += "function RugMap(hash, options)\n"
 			js += "{\n"
+			js += "	this.DEFAULT_LATITUDE = 50.0596696; /* Prague */\n"
+			js += "	this.DEFAULT_LONGITUDE = 14.4656239;\n"
+			js += "	this.DEFAULT_ZOOM = 9;\n"
 			js += "	this.map = null;\n"
-			js += "	this.markers = [];\n"
-			js += "	this.polygons = [];\n"
+			js += "	this.markers = {};\n"
+			js += "	this.marker_labels = {};\n"
+			js += "	this.polygons = {};\n"
+			js += "	this.polygon_labels = {};\n"
 			js += "	this.hash = hash;\n"
 			js += "	this.options = (typeof options !== 'undefined' ? options : {});\n"
 			js += "}\n"
 			js += "RugMap.prototype = {\n"
 			js += "	constructor: RugMap,\n"
-			js += "	addMarker: function(latitude, longitude)\n"
+			js += "	addMarker: function(name, latitude, longitude)\n"
 			js += "	{\n"
 			js += "		var marker = new google.maps.Marker({\n"
 			js += "			map: this.map,\n"
 			js += "			position: {lat: latitude, lng: longitude}\n"
 			js += "		});\n"
-			js += "		this.markers.push(marker);\n"
+			js += "		this.markers[name] = marker;\n"
 			js += "		return marker;\n"
 			js += "	},\n"
-			js += "	removeMarker: function(marker)\n"
+			js += "	addMarkerLabel: function(name, content) \n"
 			js += "	{\n"
-			js += "		marker.setMap(null);\n"
-			js += "		for (var i = 0; i < this.markers.length; i++) {\n"
-			js += "			if (marker == this.markers[i]) {\n"
-			js += "				this.markers.splice(i, 1);\n"
-			js += "				break;\n"
-			js += "			}\n"
+			js += "		var _this = this;\n"
+			js += "		var marker_label = null;\n"
+			js += "		if (this.markers.hasOwnProperty(name)) {\n"
+			js += "			marker_label = new google.maps.InfoWindow({ content: content });\n"
+			js += "			this.markers[name].addListener('click', function(event) {\n"
+			js += "				marker_label.open(_this.map, _this.markers[name]);\n"
+			js += "			});\n"
+			js += "			this.marker_labels[name] = marker_label;\n"
 			js += "		}\n"
-			js += "		return true;\n"
+			js += "		return marker_label;\n"
 			js += "	},\n"
-			js += "	clearMarkers: function()\n"
-			js += "	{\n"
-			js += "		for (var i = 0; i < this.markers.length; i++) {\n"
-			js += "			var marker = this.markers[i];\n"
-			js += "			marker.setMap(null);\n"
-			js += "		}\n"
-			js += "		this.markers = [];\n"
-			js += "		return true;\n"
-			js += "	},\n"
-			js += "	addPolygon: function(points) \n"
+			js += "	addPolygon: function(name, points) \n"
 			js += "	{\n"
 			js += "		var points_to_draw = [];\n"
 			js += "		if (points instanceof Array) {\n"
@@ -131,46 +143,34 @@ module RugBuilder
 			js += "			fillOpacity: 0.2\n"
 			js += "		});\n"
 			js += "		polygon.setMap(this.map);\n"
-			js += "		this.polygons.push(polygon);\n"
+			js += "		this.polygons[name] = polygon;\n"
 			js += "		return polygon;\n"
 			js += "	},\n"
-			js += "	removePolygon: function(polygon)\n"
+			js += "	addPolygonLabel: function(name, content) \n"
 			js += "	{\n"
-			js += "		polygon.setMap(null);\n"
-			js += "		for (var i = 0; i < this.polygons.length; i++) {\n"
-			js += "			if (polygon == this.polygons[i]) {\n"
-			js += "				this.polygons.splice(i, 1);\n"
-			js += "				break;\n"
-			js += "			}\n"
+			js += "		var _this = this;\n"
+			js += "		var polygon_label = null;\n"
+			js += "		if (this.polygons.hasOwnProperty(name)) {\n"
+			js += "			polygon_label = new google.maps.InfoWindow({ content: content });\n"
+			js += "			this.polygons[name].addListener('click', function(event) {\n"
+			js += "				polygon_label.setPosition(event.latLng);\n"
+			js += "				polygon_label.open(_this.map);\n"
+			js += "			});\n"
+			js += "			this.polygon_labels[name] = polygon_label;\n"
 			js += "		}\n"
-			js += "		return true;\n"
-			js += "	},\n"
-			js += "	clearPolygons: function()\n"
-			js += "	{\n"
-			js += "		for (var i = 0; i < this.polygons.length; i++) {\n"
-			js += "			var polygon = this.polygons[i];\n"
-			js += "			polygon.setMap(null);\n"
-			js += "		}\n"
-			js += "		this.polygons = [];\n"
-			js += "		return true;\n"
-			js += "	},\n"
-			js += "	clear: function()\n"
-			js += "	{\n"
-			js += "		this.clearMarkers();\n"
-			js += "		this.clearPolygons();\n"
-			js += "		return true;\n"
+			js += "		return polygon_label;\n"
 			js += "	},\n"
 			js += "	ready: function()\n"
 			js += "	{\n"
 			js += "		var _this = this;\n"
-			js += "		var default_latitude = (this.options.default_latitude ? this.options.default_latitude : 50.0596696); /* Prague */\n"
-			js += "		var default_longitude = (this.options.default_longitude ? this.options.default_longitude : 14.4656239);\n"
-			js += "		var default_zoom = (this.options.default_zoom ? this.options.default_zoom : 9);\n"
+			js += "		var latitude = (this.options.latitude ? this.options.latitude : this.DEFAULT_LATITUDE);\n"
+			js += "		var longitude = (this.options.longitude ? this.options.longitude : this.DEFAULT_LONGITUDE);\n"
+			js += "		var zoom = (this.options.zoom ? this.options.zoom : this.DEFAULT_ZOOM);\n"
 			js += "		var map_canvas = $('#map_' + this.hash + ' .mapbox').get(0);\n"
-			js += "		var map_position = new google.maps.LatLng(default_latitude, default_longitude);\n"
+			js += "		var map_position = new google.maps.LatLng(latitude, longitude);\n"
 			js += "		var map_options = {\n"
 			js += "			center: map_position,\n"
-			js += "			zoom: default_zoom,\n"
+			js += "			zoom: zoom,\n"
 			js += "			mapTypeId: google.maps.MapTypeId.ROADMAP,\n"
 			js += "		}\n"
 			js += "		this.map = new google.maps.Map(map_canvas, map_options);\n"
@@ -178,6 +178,9 @@ module RugBuilder
 			js += "	repair: function()\n"
 			js += "	{\n"
 			js += "		google.maps.event.trigger(this.map, 'resize');\n"
+			js += "		var latitude = (this.options.latitude ? this.options.latitude : this.DEFAULT_LATITUDE);\n"
+			js += "		var longitude = (this.options.longitude ? this.options.longitude : this.DEFAULT_LONGITUDE);\n"
+			js += "		this.map.panTo({lat: latitude, lng: longitude});\n"
 			js += "	}\n"
 			js += "}\n"
 
@@ -190,19 +193,15 @@ module RugBuilder
 			js += "var rug_map_#{@hash} = null;\n"
 			js += "$(document).ready(function() {\n"
 			js += "	rug_map_#{@hash} = new RugMap('#{@hash}', {\n"
-			js += "		default_latitude: #{( @options[:default_latitude] ? @options[:default_latitude] : 50.0596696)},\n"
-			js += "		default_longitude: #{( @options[:default_longitude] ? @options[:default_longitude] : 14.4656239)},\n"
-			js += "		default_zoom: #{( @options[:default_zoom] ? @options[:default_zoom] : 9)}\n"
+			js += "		latitude: #{@options[:latitude]},\n" if @options[:latitude]
+			js += "		longitude: #{@options[:longitude]},\n" if @options[:longitude]
+			js += "		zoom: #{@options[:zoom]}\n" if @options[:zoom]
 			js += "	});\n"
 			js += "	rug_map_#{@hash}.ready();\n"
 			js += @template.capture(self, &block).to_s
 			js += "});\n"
 
 			return js
-		end
-
-		def controller_path
-			return @template.controller.class.name.to_snake[0..-12]
 		end
 
 	end
