5c7d4a735819118451e9c8f257c2573828eb8bac
Matěj Outlý <matej.outly@clockstar.cz>
2016-02-01 13:51:14 +0100

    DateTime composition

--- rug_support/lib/rug_support/util/datetime.rb
+++ rug_support/lib/rug_support/util/datetime.rb
@@ -18,4 +18,24 @@ class DateTime
 		return (self.to_date - Date.new(self.year, 1, 1)).to_i
 	end
 
+	#
+	# Compose date and time into one datetime
+	#
+	def self.compose(date, time)
+		if time.is_a?(String) 
+			time = DateTime.parse(time)
+		end
+		time_utc = time.utc
+		result = DateTime.new(
+			date.year, 
+			date.month, 
+			date.mday, 
+			time_utc.strftime("%k").to_i, # hour
+			time_utc.strftime("%M").to_i, # minute
+			time_utc.strftime("%S").to_i # second
+		).in_time_zone(Time.zone)
+		result += (time.strftime("%:z").to_i - result.strftime("%:z").to_i).hours
+		return result
+	end
+
 end
