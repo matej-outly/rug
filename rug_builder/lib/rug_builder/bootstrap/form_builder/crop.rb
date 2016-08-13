bfde9d544f6a8a6edbefe4ec2c11674c228f83ab
Matěj Outlý <matej.outly@clockstar.cz>
2016-08-13 13:35:46 +0200

    RugBuilder - crop form element ported to bootstrap

--- rug_builder/lib/rug_builder/bootstrap/form_builder/crop.rb
+++ rug_builder/lib/rug_builder/bootstrap/form_builder/crop.rb
@@ -2,7 +2,7 @@
 # * Copyright (c) Clockstar s.r.o. All rights reserved.
 # *****************************************************************************
 # *
-# * Rug form builder - crop TODO
+# * Rug form builder - crop
 # *
 # * Author: Matěj Outlý
 # * Date  : 8. 3. 2015
@@ -14,12 +14,11 @@ module RugBuilder
 		class FormBuilder < ActionView::Helpers::FormBuilder
 
 			def crop_row(name, options = {})
+				result = ""
 				
 				if !self.options[:update_url] || (object.new_record? && !self.options[:create_url])
 					raise "Please define update and create URL."
 				end
-
-				result = "<div class=\"element\">"
 
 				# Unique hash
 				hash = Digest::SHA1.hexdigest(name.to_s)
@@ -123,7 +122,7 @@ module RugBuilder
 				result += @template.javascript_tag(js)
 
 				# Container
-				result += "<div id=\"crop_#{hash}\" class=\"field\">"
+				result += "<div id=\"crop_#{hash}\" class=\"form-group\">"
 
 				# Crop attributes
 				for attribute in [:crop_x, :crop_y, :crop_w, :crop_h]
@@ -141,7 +140,6 @@ module RugBuilder
 				# Container end
 				result += "</div>"
 
-				result += "</div>"
 				return result.html_safe
 			end
 
