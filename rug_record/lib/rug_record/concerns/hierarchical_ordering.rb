50f07d01bb44afec46b4c2c23f6af22d49f5959d
Matěj Outlý <matej.outly@clockstar.cz>
2016-11-11 18:06:47 +0100

    RugRecord - hierarchical models support moving and export to JSON

--- rug_record/lib/rug_record/concerns/hierarchical_ordering.rb
+++ rug_record/lib/rug_record/concerns/hierarchical_ordering.rb
@@ -29,8 +29,92 @@ module RugRecord
 					# Mark as ordered
 					@hierarchically_ordered = true
 
-					# Set privilege checker
-					#around_move :check_update_event
+					#
+					# Move object to relation with other object
+					#
+					define_singleton_method(:move) do |moved_id, relation, destination_id|
+						moved_object = find_by_id(moved_id)
+						destination_object = find_by_id(destination_id)
+						if moved_object && destination_object
+							if relation.to_sym == :right
+								return moved_object.move_to_right_of(destination_object)
+							elsif relation.to_sym == :left
+								return moved_object.move_to_left_of(destination_object)
+							elsif relation.to_sym == :child
+								return moved_object.move_to_child_of(destination_object)
+							else
+								return false
+							end
+						else
+							return false
+						end
+					end
+
+					#
+					# Get maximal level of existing nodes
+					#
+					define_singleton_method(:maximal_level) do
+						return maximum("depth").to_i
+					end
+
+					#
+					# Create JSON (string) representation of the hierarchical tree. 
+					# Collection of objects ordered by lft can be passed.
+					#
+					define_singleton_method(:to_hierarchical_json) do |objects = nil|
+						return ActiveSupport::JSON.encode(as_hierarchical_json(objects))
+					end
+
+					#
+					# Create JSON (object) representation of the hierarchical tree. 
+					# Collection of objects ordered by lft can be passed.
+					#
+					define_singleton_method(:as_hierarchical_json) do |objects = nil|
+						
+						# Default objects
+						objects = all.order(lft: :asc) if objects.nil?
+						
+						# Empty selection
+						return nil if objects.size == 0
+
+						# preset path with fake parent
+						path = [{id: objects.first.parent_id}]
+
+						last_object_as_json = nil
+						objects.each do |object|
+							
+							# Check if new level => have we descened or ascened? Then update path (and context) accordingly
+							if object.parent_id != path.last[:id] 
+								if path.map{ |i| i[:id] }.include?(object.parent_id) # We ascended
+									path.pop while path.last[:id] != object.parent_id # Remove the wrong trailing path elements
+								else # We descended
+									path << last_object_as_json
+								end
+							end
+
+							# Save object to next iteration where can be added to path as parent
+							last_object_as_json = object.as_json.symbolize_keys
+
+							# Add object to current context (= last item in path)
+							path.last[:children] = [] if path.last[:children].nil?
+							path.last[:children] << last_object_as_json
+
+						end
+						
+						# Result
+						return path.first[:children]
+					end
+
+					#
+					# Get all available parents
+					#
+					define_method(:available_parents) do
+						if self.new_record?
+							self.class.all.order(lft: :asc)
+						else
+							self.class.where("id <> :id", id: self.id).order(lft: :asc)
+						end
+					end
 
 				end
 
@@ -42,20 +126,15 @@ module RugRecord
 				end
 
 				#
-				# Get maximal level of existing nodes
+				# Iterate over all hierarchical items including fake (dummy) root
 				#
-				def maximal_level
-					return maximum("depth").to_i
-				end
-
 				def each_plus_dummy_with_level(objects, &block)
 					path = [nil]
 					objects.each do |o|
 						if o.parent_id != path.last
-							# we are on a new level, did we descend or ascend?
+							# We are on a new level, did we descend or ascend?
 							if path.include?(o.parent_id)
-								# remove wrong tailing paths elements
-								path.pop while path.last != o.parent_id
+								path.pop while path.last != o.parent_id # Remove wrong tailing paths elements
 							else
 								path << o.parent_id
 							end
@@ -74,17 +153,6 @@ module RugRecord
 			#
 			def hierarchically_ordered?
 				return self.class.hierarchically_ordered?
-			end
-
-			#
-			# Get all available parents
-			#
-			def available_parents
-				if self.new_record?
-					self.class.all.order(lft: :asc)
-				else
-					self.class.where("id <> :id", id: self.id).order(lft: :asc)
-				end
 			end
 
 		end
--- rug_record/lib/rug_record/concerns/ordering.rb
+++ rug_record/lib/rug_record/concerns/ordering.rb
@@ -31,6 +31,77 @@ module RugRecord
 
 					# Mark as ordered
 					@ordered = true
+					
+					#
+					# Move object to relation with other object
+					#
+					define_singleton_method(:move) do |moved_id, relation, destination_id|
+						moved_object = find_by_id(moved_id)
+						destination_object = find_by_id(destination_id)
+						if moved_object && destination_object
+							if relation.to_sym == :right
+								return moved_object.insert_at(destination_object.position + 1)
+							elsif relation.to_sym == :left
+								return moved_object.insert_at(destination_object.position)
+							else
+								return false
+							end
+						else
+							return false
+						end
+					end
+
+					#
+					# Move to specific position
+					#
+					define_method(:move_to_position) do |position|
+						insert_at(position)
+					end
+
+					#
+					# Move before some other node
+					#
+					define_method(:move_to_left_of) do |destination|
+						return if self.position.nil? || destination.position.nil?
+						if self.position < destination.position
+							position = destination.position - 1
+						else
+							position = destination.position
+						end
+						move_to_position(position)
+					end
+					
+					#
+					# Move before some other node
+					#
+					define_method(:move_to_right_of) do |destination|
+						return if self.position.nil? || destination.position.nil?
+						if self.position <= destination.position
+							position = destination.position
+						else
+							position = destination.position + 1
+						end
+						move_to_position(position)
+					end
+
+					#
+					# Get next in order
+					#
+					define_method(:next_in_order) do
+						next_model = self.class.where("position > ?", self.position).order(position: :asc).limit(1).first
+						next_model ||= self.class.order(position: :asc).first
+						return next_model
+					end
+
+					#
+					# Get previous in order
+					#
+					define_method(:prev_in_order) do
+						prev_model = self.class.where("position < ?", self.position).order(position: :desc).limit(1).first
+						prev_model ||= self.class.order(position: :asc).last
+						return prev_model
+					end
+
 				end
 
 				#
@@ -38,25 +109,6 @@ module RugRecord
 				#
 				def ordered?
 					return (@ordered == true)
-				end
-
-				#
-				# Move object to relation with other object
-				#
-				def move(moved_id, relation, destination_id)
-					moved_object = find_by_id(moved_id)
-					destination_object = find_by_id(destination_id)
-					if moved_object && destination_object
-						if relation.to_sym == :succ
-							return moved_object.insert_at(destination_object.position + 1)
-						elsif relation.to_sym == :pred
-							return moved_object.insert_at(destination_object.position)
-						else
-							return false
-						end
-					else
-						return false
-					end
 				end
 
 			end
@@ -68,57 +120,6 @@ module RugRecord
 			#
 			def ordered?
 				return self.class.ordered?
-			end
-
-			#
-			# Move to specific position
-			#
-			def move_to_position(position)
-				insert_at(position)
-			end
-
-			#
-			# Move before some other node
-			#
-			def move_to_left_of(destination)
-				return if self.position.nil? || destination.position.nil?
-				if self.position < destination.position
-					position = destination.position - 1
-				else
-					position = destination.position
-				end
-				move_to_position(position)
-			end
-			
-			#
-			# Move before some other node
-			#
-			def move_to_right_of(destination)
-				return if self.position.nil? || destination.position.nil?
-				if self.position <= destination.position
-					position = destination.position
-				else
-					position = destination.position + 1
-				end
-				move_to_position(position)
-			end
-
-			#
-			# Get next in order
-			#
-			def next_in_order
-				next_model = self.class.where("position > ?", self.position).order(position: :asc).limit(1).first
-				next_model ||= self.class.order(position: :asc).first
-				return next_model
-			end
-
-			#
-			# Get previous in order
-			#
-			def prev_in_order
-				prev_model = self.class.where("position < ?", self.position).order(position: :desc).limit(1).first
-				prev_model ||= self.class.order(position: :asc).last
-				return prev_model
 			end
 
 		end
