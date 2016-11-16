baee2ad0f720c50156dca9e3aaeaf62f286abfbf
Matěj Outlý <matej.outly@clockstar.cz>
2016-11-16 21:18:11 +0100

    Fix in CRUD generator

--- lib/generators/rug/controller/crud/templates/controller.rb
+++ lib/generators/rug/controller/crud/templates/controller.rb
@@ -11,11 +11,9 @@
 
 class <%= controller_path.to_camel %>Controller < ApplicationController
 
+	before_action :save_referrer, only: [:new, :edit]
 	before_action :set_<%= model_name.to_snake.singularize %>, only: [:show, :edit, :update, :destroy]
 
-	#
-	# Index action
-	#
 	def index
 		@filter_<%= model_name.to_snake.singularize %> = <%= model_path.to_camel.singularize %>.new(load_params_from_session)
 		@<%= model_name.to_snake.pluralize %> = <%= model_path.to_camel.singularize %>.filter(load_params_from_session.symbolize_keys).sorting(params[:sort]).page(params[:page]).per(50)
@@ -25,17 +23,11 @@ class <%= controller_path.to_camel %>Controller < ApplicationController
 		end
 	end
 
-	#
-	# Filter action
-	#
 	def filter
 		save_params_to_session(filter_params)
 		redirect_to main_app.<%= controller_path.to_snake.gsub("/", "_").pluralize %>_path
 	end
 
-	#
-	# Search action
-	#
 	def search
 		@<%= model_name.to_snake.pluralize %> = <%= model_path.to_camel.singularize %>.search(params[:q]).order(id: :asc)
 		respond_to do |format|
@@ -44,9 +36,6 @@ class <%= controller_path.to_camel %>Controller < ApplicationController
 		end
 	end
 
-	#
-	# Show action
-	#
 	def show
 		respond_to do |format|
 			format.html { render "show" }
@@ -54,27 +43,18 @@ class <%= controller_path.to_camel %>Controller < ApplicationController
 		end
 	end
 
-	#
-	# New action
-	#
 	def new
 		@<%= model_name.to_snake.singularize %> = <%= model_path.to_camel.singularize %>.new
 	end
 
-	#
-	# Edit action
-	#
 	def edit
 	end
 
-	#
-	# Create action
-	#
 	def create
 		@<%= model_name.to_snake.singularize %> = <%= model_path.to_camel.singularize %>.new(<%= model_name.to_snake.singularize %>_params)
 		if @<%= model_name.to_snake.singularize %>.save
 			respond_to do |format|
-				format.html { redirect_to main_app.<%= controller_path.to_snake.gsub("/", "_").singularize %>_path(@<%= model_name.to_snake.singularize %>), notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.create") }
+				format.html { redirect_to load_referrer, notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.create") }
 				format.json { render json: @<%= model_name.to_snake.singularize %>.id }
 			end
 		else
@@ -85,13 +65,10 @@ class <%= controller_path.to_camel %>Controller < ApplicationController
 		end
 	end
 
-	#
-	# Update action
-	#
 	def update
 		if @<%= model_name.to_snake %>.update(<%= model_name.to_snake.singularize %>_params)
 			respond_to do |format|
-				format.html { redirect_to main_app.<%= controller_path.to_snake.gsub("/", "_").singularize %>_path(@<%= model_name.to_snake.singularize %>), notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.update") }
+				format.html { redirect_to load_referrer, notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.update") }
 				format.json { render json: @<%= model_name.to_snake.singularize %>.id }
 			end
 		else
@@ -102,13 +79,10 @@ class <%= controller_path.to_camel %>Controller < ApplicationController
 		end
 	end
 
-	#
-	# Destroy action
-	#
 	def destroy
 		@<%= model_name.to_snake.singularize %>.destroy
 		respond_to do |format|
-			format.html { redirect_to main_app.<%= controller_path.to_snake.gsub("/", "_").pluralize %>_path, notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.destroy") }
+			format.html { redirect_to request.referrer, notice: I18n.t("activerecord.notices.models.<%= model_name.to_snake.singularize %>.destroy") }
 			format.json { render json: @<%= model_name.to_snake.singularize %>.id }
 		end
 	end
