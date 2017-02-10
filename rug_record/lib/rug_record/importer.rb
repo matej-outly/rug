85ff75d9fd8eace939c4f9ea75ba1d0629f2d017
Matěj Outlý <matej.outly@clockstar.cz>
2017-02-10 16:18:44 +0100

    RugRecord - imported XML can be splitted by multiple elements

--- rug_record/lib/rug_record/importer.rb
+++ rug_record/lib/rug_record/importer.rb
@@ -424,15 +424,59 @@ module RugRecord
 		# *************************************************************************
 
 		def xml_parse(data, options = {})
-			if !options[:split_by_element].nil?
+
+			# Normalize options
+			options[:split_by_elements] = {options[:split_by_element].to_s => :parse} if !options[:split_by_element].nil?
+
+			if !options[:split_by_elements].nil?
+				element_names = options[:split_by_elements].keys
+
+				# Prepare control structures
+				context = {}
+
 				Nokogiri::XML::Reader(data).each do |node|
-					if node.name == options[:split_by_element].to_s && node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
-						if (node.outer_xml.length / 1048576) <= get_xml_size_limit # Size check (big XML data cannot be parsed to DOM)
-							yield Nokogiri::XML(node.outer_xml).first_element_child
+					
+					# Something interesting found
+					if element_names.include?(node.name) && node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+						
+						# Last element found => parse it and yield with context
+						if node.name == element_names.last
+							attribute = options[:split_by_elements][node.name]
+							if attribute === :parse
+								if (node.outer_xml.length / 1048576) <= get_xml_size_limit # Size check (big XML data cannot be parsed to DOM)
+									context[node.name] = Nokogiri::XML(node.outer_xml).first_element_child
+								else
+									raise "XML data too large."
+								end
+							else
+								context[node.name] = node.attribute(attribute)
+							end
+
+							# Params for yield
+							yield_params = []
+							element_names.each do |element|
+								yield_params << context[element]
+							end
+							
+							# Yield
+							if yield_params.length == 1
+								yield yield_params.first
+							else
+								yield yield_params
+							end
+
+						# Do not parse, just save context
 						else
-							raise "XML data too large."
+							attribute = options[:split_by_elements][node.name]
+							if attribute === :parse
+								raise "Cannot parse context element, please specify single atrribute to retrieve."
+							else
+								context[node.name] = node.attribute(attribute)
+							end
 						end
+
 					end
+
 				end
 			else
 				if (data.length / 1048576) <= get_xml_size_limit # Size check (big XML data cannot be parsed to DOM)
