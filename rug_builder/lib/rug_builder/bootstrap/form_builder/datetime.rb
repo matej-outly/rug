8923f1eb2994ef5a19a21b2e838aadbb13824f28
Matěj Outlý <matej.outly@clockstar.cz>
2017-03-01 17:52:07 +0100

    RugBuilder - new form input for date time range (taken from gummy version)

--- rug_builder/lib/rug_builder/bootstrap/form_builder/datetime.rb
+++ rug_builder/lib/rug_builder/bootstrap/form_builder/datetime.rb
@@ -163,6 +163,133 @@ module RugBuilder
 				return result.html_safe
 			end
 
+			def datetime_range_picker_row(name, options = {})
+				result = ""
+
+				# Unique hash
+				hash = Digest::SHA1.hexdigest(name.to_s)
+
+				# Label
+				result += compose_label(name, options)
+
+				# Part labels
+				label_date = (options[:label_date] ? options[:label_date] : I18n.t("general.attribute.datetime_range.date"))
+				label_from = (options[:label_from] ? options[:label_from] : I18n.t("general.attribute.datetime_range.from"))
+				label_to = (options[:label_to] ? options[:label_to] : I18n.t("general.attribute.datetime_range.to"))
+				
+				# Part values
+				value = object.send(name)
+				value_date = value && value[:date] ? value[:date] : nil
+				value_from = value && value[:from] ? value[:from] : nil
+				if !value_from.blank?
+					value_from = value_from.strftime("%k:%M")
+				end
+				value_to = value && value[:to] ? value[:to] : nil
+				if !value_to.blank?
+					value_to = value_to.strftime("%k:%M")
+				end
+				
+				# Java Script
+				result += @template.javascript_tag(%{
+					function datetime_range_picker_#{hash}_ready()
+					{
+						$('#datetime_range_picker_#{hash} .date').pikaday({ 
+							firstDay: 1,
+							format: 'YYYY-MM-DD',
+							i18n: {
+								previousMonth : '#{I18n.t("views.calendar.prev_month")}',
+								nextMonth     : '#{I18n.t("views.calendar.next_month")}',
+								months        : ['#{I18n.t("views.calendar.months.january")}','#{I18n.t("views.calendar.months.february")}','#{I18n.t("views.calendar.months.march")}','#{I18n.t("views.calendar.months.april")}','#{I18n.t("views.calendar.months.may")}','#{I18n.t("views.calendar.months.june")}','#{I18n.t("views.calendar.months.july")}','#{I18n.t("views.calendar.months.august")}','#{I18n.t("views.calendar.months.september")}','#{I18n.t("views.calendar.months.october")}','#{I18n.t("views.calendar.months.november")}','#{I18n.t("views.calendar.months.december")}'],
+								weekdays      : ['#{I18n.t("views.calendar.days.sunday")}','#{I18n.t("views.calendar.days.monday")}','#{I18n.t("views.calendar.days.tuesday")}','#{I18n.t("views.calendar.days.wednesday")}','#{I18n.t("views.calendar.days.thursday")}','#{I18n.t("views.calendar.days.friday")}','#{I18n.t("views.calendar.days.saturday")}'],
+								weekdaysShort : ['#{I18n.t("views.calendar.short_days.sunday")}','#{I18n.t("views.calendar.short_days.monday")}','#{I18n.t("views.calendar.short_days.tuesday")}','#{I18n.t("views.calendar.short_days.wednesday")}','#{I18n.t("views.calendar.short_days.thursday")}','#{I18n.t("views.calendar.short_days.friday")}','#{I18n.t("views.calendar.short_days.saturday")}']
+							}
+						});
+						$('#datetime_range_picker_#{hash} .from').clockpicker({
+							placement: 'bottom',
+							align: 'left',
+							autoclose: true
+						});
+						$('#datetime_range_picker_#{hash} .to').clockpicker({
+							placement: 'bottom',
+							align: 'left',
+							autoclose: true
+						});
+					}
+					$(document).ready(datetime_range_picker_#{hash}_ready);
+				})
+				
+				# Field options
+				klass = []
+				klass << "form-control"
+				klass << options[:class] if !options[:class].nil?
+
+				# Column width
+				columns_count = (3 - [options[:date], options[:from], options[:to]].count(false))
+				if columns_count > 0
+					column_width = 12 / columns_count
+				else
+					column_width = 12
+				end
+				
+				if options[:date] != false
+					result_date = %{
+						<div class="col-sm-#{column_width}">
+							<div class="input-group">
+								<div class="input-group-addon">#{label_date.upcase_first}</div>
+								#{@template.text_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][date]", value_date, class: klass.dup.concat(["date"]))}
+							</div>
+						</div>
+					}
+				else
+					result_date = %{
+						#{@template.hidden_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][date]", value_date)}
+					}
+				end
+
+				if options[:from] != false
+					result_from = %{
+						<div class="col-sm-#{column_width}">
+							<div class="input-group">
+								<div class="input-group-addon">#{label_from.upcase_first}</div>
+								#{@template.text_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][from]", value_from, class: klass.dup.concat(["from"]))}
+							</div>
+						</div>
+					}
+				else
+					result_from = %{
+						#{@template.hidden_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][from]", value_from)}
+					}
+				end
+
+				if options[:to] != false
+					result_to = %{
+						<div class="col-sm-#{column_width}">
+							<div class="input-group">
+								<div class="input-group-addon">#{label_to.upcase_first}</div>
+								#{@template.text_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][to]", value_to, class: klass.dup.concat(["to"]))}
+							</div>
+						</div>
+					}
+				else
+					result_to = %{
+						#{@template.hidden_field_tag("#{object.class.model_name.param_key}[#{name.to_s}][to]", value_to)}
+					}
+				end
+
+				result += %{
+					<div class="form-horizontal">
+						<div id="datetime_range_picker_#{hash}" class="form-group #{(has_error?(name) ? "has-error" : "")}">
+							#{result_date}
+							#{result_from}
+							#{result_to}
+							#{errors(name, class: "col-sm-12")}
+						</div>
+					</div>
+				}
+
+				return result.html_safe
+			end
+
 			def duration_row(name, options = {})
 				result = ""
 
