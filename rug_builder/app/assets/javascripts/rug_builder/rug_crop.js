03317169101609e5fe144a7560b8666e68d5b833
Jaroslav Mlejnek <jar.gml55@gmail.com>
2017-04-04 21:34:04 +0200

    Rug Builder Crop use croppable instead of jcrop

---
+++ rug_builder/app/assets/javascripts/rug_builder/rug_crop.js
@@ -0,0 +1,66 @@
+/*****************************************************************************/
+/* Copyright (c) Clockstar s.r.o. All rights reserved.                       */
+/*****************************************************************************/
+/*                                                                           */
+/* Rug Crop                                                                  */
+/*                                                                           */
+/* Author:                                                                   */
+/* Date  : 4. 4. 2017                                                        */
+/*                                                                           */
+/*****************************************************************************/
+
+
+function RugCrop(hash, options)
+{
+	this.hash = hash;
+	this.$crop = null;
+	this.$cropX = null;
+	this.$cropY = null;
+	this.$cropW = null;
+	this.$cropH = null;
+
+	this.options = $.extend({
+		aspectRatio: null,
+		initial: null,
+	}, options);
+}
+RugCrop.prototype = {
+	constructor: RugCrop,
+
+	//
+	// Cropped area has been changed
+	//
+	onUpdate: function(coords)
+	{
+		this.$cropX.val(coords.x);
+		this.$cropY.val(coords.y);
+		this.$cropW.val(coords.width);
+		this.$cropH.val(coords.height);
+	},
+
+	//
+	// Reload plugin
+	//
+	reload: function()
+	{
+		console.log("RugCrop::reload not implemented");
+	},
+
+	//
+	// Initialize
+	//
+	ready: function()
+	{
+		this.$crop = $('#crop_' + this.hash + ' .cropbox img');
+		this.$cropX = $('#crop_' + this.hash + ' .crop_x');
+		this.$cropY = $('#crop_' + this.hash + ' .crop_y');
+		this.$cropW = $('#crop_' + this.hash + ' .crop_w');
+		this.$cropH = $('#crop_' + this.hash + ' .crop_h');
+
+		this.$crop.croppable({
+			aspectRatio: this.options.aspectRatio,
+			initial: this.options.initial,
+			updated: this.onUpdate.bind(this),
+		});
+	},
+}
--- rug_builder/lib/rug_builder/bootstrap/form_builder/crop.rb
+++ rug_builder/lib/rug_builder/bootstrap/form_builder/crop.rb
@@ -15,7 +15,7 @@ module RugBuilder
 
 			def crop_row(name, options = {})
 				result = ""
-				
+
 				if !self.options[:update_url] || (object.new_record? && !self.options[:create_url])
 					raise "Please define update and create URL."
 				end
@@ -36,7 +36,7 @@ module RugBuilder
 				# Cropped and croppable styles
 				cropped_style = object.send("#{name.to_s}_cropped_style".to_sym)
 				croppable_style = object.send("#{name.to_s}_croppable_style".to_sym)
-				
+
 				# Cropped style geometry
 				cropped_style_geometry = value.styles[cropped_style.to_sym].geometry
 				splitted_cropped_style_geometry = cropped_style_geometry[0..-2].split("x")
@@ -54,77 +54,84 @@ module RugBuilder
 				end
 
 				# Java Script
-				js = ""
+				# js = ""
 
-				js += "function crop_#{hash}_update_coords(coords) \n"
-				js += "{\n"
-				js += "	$('#crop_#{hash} .crop_x').val(Math.round(coords.x));\n"
-				js += "	$('#crop_#{hash} .crop_y').val(Math.round(coords.y));\n"
-				js += "	$('#crop_#{hash} .crop_w').val(Math.round(coords.w));\n"
-				js += "	$('#crop_#{hash} .crop_h').val(Math.round(coords.h));\n"
-				js += "}\n"
-				
-				js += "function crop_#{hash}_reload_jcrop()\n"
-				js += "{\n"
-				js += "	var img = $('#crop_#{hash} .cropbox img');\n"
-				js += "	var jcrop_api = img.data('Jcrop');\n"
-				js += "	if (jcrop_api) {\n"
-				js += "		jcrop_api.destroy();\n"
-				js += "	}\n"
-				js += "	var natural_width = img.get(0).naturalWidth;\n"
-				js += "	var natural_height = img.get(0).naturalHeight;\n"
-				js += "	img.Jcrop({\n"
-				js += "		trueSize: [natural_width, natural_height],\n"
-				js += "		onChange: crop_#{hash}_update_coords,\n"
-				js += "		onSelect: crop_#{hash}_update_coords,\n"
-				if already_cropped # TODO Dynamic from hidden inputs
-					js += "		setSelect: [#{(crop_x <= crop_w ? crop_x : crop_x + crop_w)}, #{(crop_y <= crop_h ? crop_y : crop_y + crop_h)}, #{crop_w}, #{crop_h}],\n" # crop_x+crop_w / crop_y+crop_h: Hack overriding bug in jCrop
-				else
-					js += "		setSelect: [0, 0, #{cropped_style_width}, #{cropped_style_height}],\n"
-				end
-				if cropped_style_aspect_ratio
-					js += "		aspectRatio: #{cropped_style_aspect_ratio},\n"
-				end
-				js += "	});\n"
-				js += "}\n"
-				
-				js += "function crop_#{hash}_reload(id)\n"
-				js += "{\n"
-				js += "	$.ajax({\n"
-				js += "		dataType: 'json',\n"
-				js += "		url: '#{RugSupport::PathResolver.new(@template).resolve(self.options[:update_url], ":id")}'.replace(':id', id),\n" # Update URL is similar to show URL
-				js += "		success: function(callback) {\n"
-				js += "			if (callback && callback.#{name.to_s}_url) {\n"
-				js += "				var src = callback.#{name.to_s}_url.replace('/original/', '/#{croppable_style.to_s}/');\n"
-				js += "				$('#crop_#{hash} .cropbox').html('<img src=\\'' + src + '\\' />');\n"
-				js += "				$('#crop_#{hash} .cropbox img').load(crop_#{hash}_reload_jcrop);\n"
-				js += "			}\n"
-				js += "		}\n"
-				js += "	});\n"
-				js += "}\n"
+				# js += "function crop_#{hash}_update_coords(coords) \n"
+				# js += "{\n"
+				# js += "	$('#crop_#{hash} .crop_x').val(coords.x);\n"
+				# js += "	$('#crop_#{hash} .crop_y').val(coords.y);\n"
+				# js += "	$('#crop_#{hash} .crop_w').val(coords.width);\n"
+				# js += "	$('#crop_#{hash} .crop_h').val(coords.height);\n"
+				# js += "}\n"
 
-				js += "function crop_#{hash}_ready()\n"
-				js += "{\n"
-				js += "	$('#crop_#{hash} .cropbox img').load(crop_#{hash}_reload_jcrop);\n"
-				js += "	if ($('#crop_#{hash} .cropbox img').length > 0) crop_#{hash}_reload_jcrop();\n"
-				js += "}\n"
+				# js += "function crop_#{hash}_reload_jcrop()\n"
+				# js += "{\n"
+				# js += "	var img = $('#crop_#{hash} .cropbox img');\n"
+				# js += "	var jcrop_api = img.data('Jcrop');\n"
+				# js += "	if (jcrop_api) {\n"
+				# js += "		jcrop_api.destroy();\n"
+				# js += "	}\n"
+				# js += "	var natural_width = img.get(0).naturalWidth;\n"
+				# js += "	var natural_height = img.get(0).naturalHeight;\n"
+				# js += "	img.Jcrop({\n"
+				# js += "		trueSize: [natural_width, natural_height],\n"
+				# js += "		onChange: crop_#{hash}_update_coords,\n"
+				# js += "		onSelect: crop_#{hash}_update_coords,\n"
+				# if already_cropped # TODO Dynamic from hidden inputs
+				# 	js += "		setSelect: [#{(crop_x <= crop_w ? crop_x : crop_x + crop_w)}, #{(crop_y <= crop_h ? crop_y : crop_y + crop_h)}, #{crop_w}, #{crop_h}],\n" # crop_x+crop_w / crop_y+crop_h: Hack overriding bug in jCrop
+				# else
+				# 	js += "		setSelect: [0, 0, #{cropped_style_width}, #{cropped_style_height}],\n"
+				# end
+				# if cropped_style_aspect_ratio
+				# 	js += "		aspectRatio: #{cropped_style_aspect_ratio},\n"
+				# end
+				# js += "	});\n"
+				# js += "}\n"
 
-				js += "$(document).ready(crop_#{hash}_ready);\n"
+				# js += "function crop_#{hash}_reload(id)\n"
+				# js += "{\n"
+				# js += "	$.ajax({\n"
+				# js += "		dataType: 'json',\n"
+				# js += "		url: '#{RugSupport::PathResolver.new(@template).resolve(self.options[:update_url], ":id")}'.replace(':id', id),\n" # Update URL is similar to show URL
+				# js += "		success: function(callback) {\n"
+				# js += "			if (callback && callback.#{name.to_s}_url) {\n"
+				# js += "				var src = callback.#{name.to_s}_url.replace('/original/', '/#{croppable_style.to_s}/');\n"
+				# js += "				$('#crop_#{hash} .cropbox').html('<img src=\\'' + src + '\\' />');\n"
+				# js += "				$('#crop_#{hash} .cropbox img').load(crop_#{hash}_reload_jcrop);\n"
+				# js += "			}\n"
+				# js += "		}\n"
+				# js += "	});\n"
+				# js += "}\n"
 
-				result += @template.javascript_tag(js)
+				# js += "function crop_#{hash}_ready()\n"
+				# js += "{\n"
+				# js += "	$('#crop_#{hash} .cropbox img').load(crop_#{hash}_reload_jcrop);\n"
+				# js += "	if ($('#crop_#{hash} .cropbox img').length > 0) crop_#{hash}_reload_jcrop();\n"
+				# js += "}\n"
+
+				# js += "$(document).ready(crop_#{hash}_ready);\n"
+
+				result += @template.javascript_tag(%{
+					var rug_crop_#{hash} = null;
+					$(document).ready(function() {
+						rug_crop_#{hash} = new RugCrop('#{hash}', {
+							aspectRatio: #{cropped_style_aspect_ratio ? cropped_style_aspect_ratio : "null"},
+							initial: {
+								x: #{already_cropped ? crop_x : 0},
+								y: #{already_cropped ? crop_y : 0},
+								width: #{already_cropped ? crop_w : cropped_style_width},
+								height: #{already_cropped ? crop_h : cropped_style_height},
+							}
+						});
+						rug_crop_#{hash}.ready();
+					});
+				})
 
 				# Container
 				result += "<div id=\"crop_#{hash}\" class=\"form-group\">"
 
 				# Label
 				result += label_for(name, options)
-				#if !options[:label].nil? # ????
-				#	if options[:label] != false
-				#		result += label(name, options[:label])
-				#	end
-				#else
-				#	result += label("#{name.to_s}_crop".to_sym)
-				#end
 
 				# Crop attributes
 				for attribute in [:crop_x, :crop_y, :crop_w, :crop_h]
