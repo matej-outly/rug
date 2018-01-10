2638d3e5dfea4448e8007b35a2a1dbd4d111097d
Matěj Outlý <matej.outly@clockstar.cz>
2018-01-10 15:59:42 +0100

    RugSupport - conversion from integer to roman number

--- rug_support/lib/rug_support/util/integer.rb
+++ rug_support/lib/rug_support/util/integer.rb
@@ -138,4 +138,33 @@ class Integer
 		return (minus + result).trim
 	end
 
+	ROMAN_NUMBERS = {
+		1000 => "M",  
+		 900 => "CM",  
+		 500 => "D",  
+		 400 => "CD",
+		 100 => "C",  
+		  90 => "XC",  
+		  50 => "L",  
+		  40 => "XL",  
+		  10 => "X",  
+		   9 => "IX",  
+		   5 => "V",  
+		   4 => "IV",  
+		   1 => "I",  
+	}
+
+	#
+	# Convert number to Roman format
+	#
+	def to_roman
+		number = self
+		result = ""
+		ROMAN_NUMBERS.each do |value, letter|
+			result << letter * (number / value)
+			number = number % value
+		end
+		return result
+	end
+
 end
